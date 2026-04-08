63f4f673c91a30d8ead2ebb5f4b78374d48c04f7
WuGambinos <69660261+WuGambinos@users.noreply.github.com>
2026-04-08 18:30:15 -0400

    remove wasm .gitignore

---
+++ crabboy-wasm/web/crabboy_wasm.d.ts
@@ -0,0 +1,86 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export enum Key {
+    Right = 0,
+    Left = 1,
+    Up = 2,
+    Down = 3,
+    A = 4,
+    B = 5,
+    Start = 6,
+    Select = 7,
+}
+
+export class Rgb {
+    private constructor();
+    free(): void;
+    [Symbol.dispose](): void;
+}
+
+export class WebGameBoy {
+    free(): void;
+    [Symbol.dispose](): void;
+    boot(rom: Uint8Array): void;
+    draw(): void;
+    constructor();
+    on_button_down(value: number): void;
+    on_button_up(value: number): void;
+    on_key_down(value: any): void;
+    on_key_up(value: any): void;
+    render_frame(): void;
+    reset(): void;
+    run(): void;
+}
+
+export function button_value_to_joypad_key(value: number): Key | undefined;
+
+export function js_value_to_joypad_key(value: any): Key | undefined;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly __wbg_webgameboy_free: (a: number, b: number) => void;
+    readonly button_value_to_joypad_key: (a: number) => number;
+    readonly js_value_to_joypad_key: (a: any) => number;
+    readonly webgameboy_boot: (a: number, b: number, c: number) => void;
+    readonly webgameboy_draw: (a: number) => void;
+    readonly webgameboy_new: () => number;
+    readonly webgameboy_on_button_down: (a: number, b: number) => void;
+    readonly webgameboy_on_button_up: (a: number, b: number) => void;
+    readonly webgameboy_on_key_down: (a: number, b: any) => void;
+    readonly webgameboy_on_key_up: (a: number, b: any) => void;
+    readonly webgameboy_render_frame: (a: number) => void;
+    readonly webgameboy_reset: (a: number) => void;
+    readonly webgameboy_run: (a: number) => void;
+    readonly __wbg_rgb_free: (a: number, b: number) => void;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __externref_table_alloc: () => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __wbindgen_exn_store: (a: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ crabboy-wasm/web/crabboy_wasm_bg.wasm.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_webgameboy_free: (a: number, b: number) => void;
+export const button_value_to_joypad_key: (a: number) => number;
+export const js_value_to_joypad_key: (a: any) => number;
+export const webgameboy_boot: (a: number, b: number, c: number) => void;
+export const webgameboy_draw: (a: number) => void;
+export const webgameboy_new: () => number;
+export const webgameboy_on_button_down: (a: number, b: number) => void;
+export const webgameboy_on_button_up: (a: number, b: number) => void;
+export const webgameboy_on_key_down: (a: number, b: any) => void;
+export const webgameboy_on_key_up: (a: number, b: any) => void;
+export const webgameboy_render_frame: (a: number) => void;
+export const webgameboy_reset: (a: number) => void;
+export const webgameboy_run: (a: number) => void;
+export const __wbg_rgb_free: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_start: () => void;
